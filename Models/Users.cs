bdc05660b289e90767c275ffa572e2d3336aab3c
Precious Nyasulu <preciousnyasulu441@gmail.com>
2024-02-01 02:39:24 +0200

    feat(Models): added Users Model

---
+++ Models/Users.cs
@@ -0,0 +1,18 @@
+﻿namespace NaLib;
+
+public class Users
+{
+    public int UserId { get; set; }
+    
+    public string UserName { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Email { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    
+}
