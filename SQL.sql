d37cdf6d5a76c3dcc307876477a2ba8c178a0ccc
PreciousNyasulu <preciousnyasulu441@gmail.com>
2024-02-02 02:24:42 +0200

    ref(SQL): updated table queries

--- SQL.sql
+++ SQL.sql
@@ -1,8 +1,18 @@
 CREATE TABLE public.users (
-	id int4 NOT NULL,
-	firstname varchar NULL,
-	lastname varchar NULL,
-	username varchar NULL,
-	email varchar NULL,
-	phonenumber varchar NULL
+    id SERIAL PRIMARY KEY,
+    firstname varchar NULL,
+    lastname varchar NULL,
+    username varchar NULL,
+    email varchar NULL,
+    phonenumber varchar NULL,
+    "role" varchar NULL,
+    "password" varchar NULL
 );
+
+
+CREATE SEQUENCE basic_sequence
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
