f2c119a57f719d894a1f55e4940766eb0e2e86bc
preciousnyasulu <preciousnyasulu441@gmail.com>
2024-02-05 10:46:35 +0200

    feat(Models): made the model attributes nullable

--- Models/LibraryResource.cs
+++ Models/LibraryResource.cs
@@ -1,22 +1,23 @@
+namespace NaLib.Models;
 public class LibraryResource
 {
     public int ResourceId { get; set; }
 
-    public string Title { get; set; }
+    public string? Title { get; set; }
 
-    public string Author { get; set; }
+    public string? Author { get; set; }
 
     public int TotalCopies { get; set; }
 
     public int AvailableCopies { get; set; }
 
-    public string ISBN { get; set; }
+    public string? ISBN { get; set; }
 
     public int PublicationYear { get; set; }
 
-    public string Genre { get; set; }
+    public string? Genre { get; set; }
 
-    public string Publisher { get; set; }
+    public string? Publisher { get; set; }
 
-    public string Description { get; set; }
+    public string? Description { get; set; }
 }
