da0cadd9f5225520748fb4df4d1f02b6bbb00458
PreciousNyasulu <preciousnyasulu441@gmail.com>
2024-02-05 03:47:38 +0200

    feat(Models): added Library resource, member models

---
+++ Models/LibraryMember.cs
@@ -0,0 +1,16 @@
+public class LibraryMember
+{
+    public int MemberId { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Email { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public string Address { get; set; }
+
+    public DateTime RegistrationDate { get; set; }
+}
---
+++ Models/LibraryResource.cs
@@ -0,0 +1,22 @@
+public class LibraryResource
+{
+    public int ResourceId { get; set; }
+
+    public string Title { get; set; }
+
+    public string Author { get; set; }
+
+    public int TotalCopies { get; set; }
+
+    public int AvailableCopies { get; set; }
+
+    public string ISBN { get; set; }
+
+    public int PublicationYear { get; set; }
+
+    public string Genre { get; set; }
+
+    public string Publisher { get; set; }
+
+    public string Description { get; set; }
+}
