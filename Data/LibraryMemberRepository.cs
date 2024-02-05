74cee57efc984bf1ad19f41ea9f4070d50dddcad
PreciousNyasulu <preciousnyasulu441@gmail.com>
2024-02-05 03:48:44 +0200

    feat(Data): added library resource, member repository

---
+++ Data/LibraryMemberRepository.cs
@@ -0,0 +1,29 @@
+
+namespace NaLib;
+public class LibraryMemberRepository : IRepository<LibraryMember>
+{
+    public void Add(LibraryMember Entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Exists(LibraryMember Entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<LibraryMember> Get(LibraryMember Entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<LibraryMember> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<LibraryMember> Update(LibraryMember Entity)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Data/LibraryResourceRepository.cs
@@ -0,0 +1,29 @@
+
+namespace NaLib;
+public class LibraryResourceRepository : IRepository<LibraryResource>
+{
+    public void Add(LibraryResource Entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Exists(LibraryResource Entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<LibraryResource> Get(LibraryResource Entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<LibraryResource> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<LibraryResource> Update(LibraryResource Entity)
+    {
+        throw new NotImplementedException();
+    }
+}
